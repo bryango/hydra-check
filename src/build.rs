dec1ffc48bbea4f705ed2fe1ae69e007ff7bbfc3
Bryan Lai <bryanlais@gmail.com>
2024-10-23 11:40:47 +0800

    Add a basic fetcher for build status

--- src/build.rs
+++ src/build.rs
@@ -1,11 +1,17 @@
-use std::fmt::Display;
+use std::{fmt::Display, time::Duration};
 
 use serde::Serialize;
 use serde_with::{skip_serializing_none, SerializeDisplay};
 
+use crate::ResolvedArgs;
+
 #[skip_serializing_none]
-#[derive(Serialize, Debug)]
-struct BuildStatus {
+#[derive(Serialize, Debug, Default)]
+pub struct BuildStatus {
+    #[serde(skip)]
+    package: String,
+    #[serde(skip)]
+    args: ResolvedArgs,
     icon: StatusIcon,
     success: bool,
     status: String,
@@ -15,12 +21,45 @@ struct BuildStatus {
     name: String,
     arch: String,
     evals: bool,
+    url: String,
 }
 
-#[derive(SerializeDisplay, Debug, Clone)]
+impl BuildStatus {
+    fn from_package_with_args(package: String, args: ResolvedArgs) -> Self {
+        //
+        // Examples:
+        // - https://hydra.nixos.org/job/nixos/release-19.09/nixpkgs.hello.x86_64-linux/latest
+        // - https://hydra.nixos.org/job/nixos/release-19.09/nixos.tests.installer.simpleUefiGrub.aarch64-linux
+        // - https://hydra.nixos.org/job/nixpkgs/trunk/hello.x86_64-linux/all
+        //
+        // There is also {url}/all which is a lot slower.
+        //
+        let url = format!("https://hydra.nixos.org/job/{}/{}", args.jobset, package);
+        Self {
+            package,
+            args,
+            url,
+            ..Default::default()
+        }
+    }
+
+    fn fetch_data(self) -> anyhow::Result<String> {
+        let text = reqwest::blocking::Client::builder()
+            .timeout(Duration::from_secs(20))
+            .build()?
+            .get(self.url)
+            .send()?
+            .error_for_status()?
+            .text()?;
+        Ok(text)
+    }
+}
+
+#[derive(SerializeDisplay, Debug, Clone, Default)]
 enum StatusIcon {
     Success,
     Failure,
+    #[default]
     Unknown,
 }
 
