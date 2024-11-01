f593c9e6fa754b29b4359160c302357060cab776
Bryan Lai <bryanlais@gmail.com>
2024-11-01 21:18:58 +0800

    Split out parse_input_changes from fetch_and_read
    ... of EvalDetails

--- src/eval.rs
+++ src/eval.rs
@@ -5,6 +5,7 @@ use colored::Colorize;
 use indexmap::IndexMap;
 use log::{info, warn};
 use regex::Regex;
+use scraper::Html;
 use serde::Serialize;
 use serde_json::Value;
 use serde_with::skip_serializing_none;
@@ -142,6 +143,94 @@ impl<'a> From<&'a Evaluation> for EvalDetails<'a> {
 }
 
 impl<'a> EvalDetails<'a> {
+    fn parse_input_changes(doc: &Html) -> anyhow::Result<Vec<EvalInputChanges>> {
+        let tables = doc.find_all("div#tabs-inputs table");
+        let err = || {
+            anyhow!(
+                "could not parse the table of changed inputs in {:?}",
+                tables.iter().map(|x| x.html()).collect::<Vec<_>>()
+            )
+        };
+        // table of input changes:
+        let table = tables.get(1).ok_or_else(err)?;
+        let thead: Vec<String> = table
+            .find("tr")?
+            .find_all("th")
+            .iter()
+            .map(|x| x.text().collect())
+            .collect();
+        if !thead
+            .iter()
+            .all(|x| x.trim().contains("Input") || x.trim().contains("Changes"))
+        {
+            bail!(err());
+        }
+        let tbody = table.find_all("tr");
+        let rows = tbody.get(1..).ok_or_else(err)?;
+        let mut input_changes = Vec::new();
+        for row in rows {
+            let columns = row.find_all("td");
+            let mut columns = columns.iter();
+            let input: String = columns.next().ok_or_else(err)?.text().collect();
+            let input = input.trim().to_string();
+
+            let changes = columns.next().ok_or_else(err)?;
+            let description: String = changes.text().collect();
+            let description = description.trim().to_string();
+
+            // the following entries are non-essential,
+            // so we avoid using `?` for premature exits
+            let url = changes
+                .find("a")
+                .ok()
+                .and_then(|x| x.attr("href"))
+                .map(|x| x.to_string());
+
+            let revs = if let Some(url) = &url {
+                // note that the returned url is not deterministic:
+                // the position of the query parameters may float around
+                let [rev1, rev2] = ["rev1", "rev2"].map(|rev_spec| {
+                    let re = format!("^.*{rev_spec}=([0-9a-z]+).*$");
+                    match Regex::new(&re).unwrap().captures(url).map(|x| x.extract()) {
+                        Some((_, [rev])) if !rev.is_empty() => Some(rev.to_string()),
+                        _ => None,
+                    }
+                });
+
+                match (rev1, rev2) {
+                    (Some(rev1), Some(rev2)) => Some((rev1, rev2)),
+                    _ => None,
+                }
+            } else {
+                None
+            };
+
+            let short_revs = if !description.is_empty() {
+                match Regex::new("^([0-9a-z]+) to ([0-9a-z]+)$")
+                    .unwrap()
+                    .captures(&description)
+                    .map(|x| x.extract())
+                {
+                    Some((_, [rev1, rev2])) if (!rev1.is_empty()) && (!rev2.is_empty()) => {
+                        Some((rev1.to_string(), rev2.to_string()))
+                    }
+                    _ => None,
+                }
+            } else {
+                None
+            };
+
+            input_changes.push(EvalInputChanges {
+                input,
+                description,
+                url,
+                revs,
+                short_revs,
+            });
+        }
+        Ok(input_changes)
+    }
+
     fn fetch_and_read(self) -> anyhow::Result<Self> {
         let doc = self.fetch_document()?;
         let tbody = match self.find_tbody(&doc, "div#tabs-inputs") {
@@ -186,95 +275,8 @@ impl<'a> EvalDetails<'a> {
                 store_path: store_path.to_owned(),
             });
         }
-        let input_changes = || -> anyhow::Result<Vec<EvalInputChanges>> {
-            let tables = doc.find_all("div#tabs-inputs table");
-            let err = || {
-                anyhow!(
-                    "could not parse the table of changed inputs in {:?}",
-                    tables.iter().map(|x| x.html()).collect::<Vec<_>>()
-                )
-            };
-            // table of input changes:
-            let table = tables.get(1).ok_or_else(err)?;
-            let thead: Vec<String> = table
-                .find("tr")?
-                .find_all("th")
-                .iter()
-                .map(|x| x.text().collect())
-                .collect();
-            if !thead
-                .iter()
-                .all(|x| x.trim().contains("Input") || x.trim().contains("Changes"))
-            {
-                bail!(err());
-            }
-            let tbody = table.find_all("tr");
-            let rows = tbody.get(1..).ok_or_else(err)?;
-            let mut input_changes = Vec::new();
-            for row in rows {
-                let columns = row.find_all("td");
-                let mut columns = columns.iter();
-                let input: String = columns.next().ok_or_else(err)?.text().collect();
-                let input = input.trim().to_string();
 
-                let changes = columns.next().ok_or_else(err)?;
-                let description: String = changes.text().collect();
-                let description = description.trim().to_string();
-
-                // the following entries are non-essential,
-                // so we avoid using `?` for premature exits
-                let url = changes
-                    .find("a")
-                    .ok()
-                    .and_then(|x| x.attr("href"))
-                    .map(|x| x.to_string());
-
-                let revs = if let Some(url) = &url {
-                    // note that the returned url is not deterministic:
-                    // the position of the query parameters may float around
-                    let [rev1, rev2] = ["rev1", "rev2"].map(|rev_spec| {
-                        let re = format!("^.*{rev_spec}=([0-9a-z]+).*$");
-                        match Regex::new(&re).unwrap().captures(url).map(|x| x.extract()) {
-                            Some((_, [rev])) if !rev.is_empty() => Some(rev.to_string()),
-                            _ => None,
-                        }
-                    });
-
-                    match (rev1, rev2) {
-                        (Some(rev1), Some(rev2)) => Some((rev1, rev2)),
-                        _ => None,
-                    }
-                } else {
-                    None
-                };
-
-                let short_revs = if !description.is_empty() {
-                    match Regex::new("^([0-9a-z]+) to ([0-9a-z]+)$")
-                        .unwrap()
-                        .captures(&description)
-                        .map(|x| x.extract())
-                    {
-                        Some((_, [rev1, rev2])) if (!rev1.is_empty()) && (!rev2.is_empty()) => {
-                            Some((rev1.to_string(), rev2.to_string()))
-                        }
-                        _ => None,
-                    }
-                } else {
-                    None
-                };
-
-                input_changes.push(EvalInputChanges {
-                    input,
-                    description,
-                    url,
-                    revs,
-                    short_revs,
-                });
-            }
-            Ok(input_changes)
-        };
-
-        let changes = input_changes().unwrap_or_else(|err| {
+        let changes = Self::parse_input_changes(&doc).unwrap_or_else(|err| {
             warn!("{}\n{}", err, err.backtrace());
             vec![]
         });
