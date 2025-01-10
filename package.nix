f1c067c1fd41d8c96542b8253b727a5ebb29d204
Bryan Lai <bryanlais@gmail.com>
2025-01-10 18:36:11 +0800

    feat: generate dev version numbers automatically

--- package.nix
+++ package.nix
@@ -3,7 +3,8 @@
   hydra-check,
   rustPlatform,
   versionCheckHook,
-}:
+  flake ? { },
+}@args:
 
 hydra-check.overrideAttrs (
   {
@@ -12,13 +13,41 @@ hydra-check.overrideAttrs (
     ...
   }:
   {
-    version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+    version =
+      let
+        packageVersion = with builtins; (fromTOML (readFile ./Cargo.toml)).package.version;
+
+        # append git revision to the version string, if available
+        revSuffix =
+          if (flake ? dirtyShortRev) then
+            "-g${flake.dirtyShortRev}"
+          else if (flake ? shortRev) then
+            "-g${flake.shortRev}-unstable"
+          else
+            "-unstable";
+
+        # append last modified date to the version string, if available
+        inherit (builtins) substring;
+        epoch = "19700101";
+        date = substring 0 8 (flake.lastModifiedDate or epoch);
+        dateSuffix = lib.optionalString (date != epoch) (
+          let
+            inherit (flake) lastModifiedDate;
+            year = substring 0 4 lastModifiedDate;
+            month = substring 4 2 lastModifiedDate;
+            day = substring 6 2 lastModifiedDate;
+          in
+          "-${year}-${month}-${day}"
+        );
+      in
+      "${packageVersion}${revSuffix}${dateSuffix}";
 
     # `builtins.path` works well with lazy trees
-    src = builtins.path {
-      name = "hydra-check-source";
-      path = ./.;
-    };
+    src =
+      args.flake or (builtins.path {
+        name = "hydra-check-source";
+        path = ./.;
+      });
 
     cargoDeps = rustPlatform.importCargoLock {
       lockFile = builtins.path {
