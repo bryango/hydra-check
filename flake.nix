7a8f98576dbe1fbe57335648e969a7512ca6eb4b
bryango <bryango@users.noreply.github.com>
2026-09-08 00:33:00 +0800

    ci: drop x86_64-darwin

--- flake.nix
+++ flake.nix
@@ -17,51 +17,57 @@
       flake-utils,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages = {
-          hydra-check = pkgs.callPackage ./package.nix { source = self; };
-          default = self.packages.${system}.hydra-check;
-        };
+    flake-utils.lib.eachSystem
+      [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ]
+      (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          packages = {
+            hydra-check = pkgs.callPackage ./package.nix { source = self; };
+            default = self.packages.${system}.hydra-check;
+          };
 
-        devShells.default =
-          (self.packages.${system}.hydra-check.override {
-            # prevent dependence on the source ./.; see ./package.nix
-            source = null;
-          }).overrideAttrs
-            (
-              {
-                nativeBuildInputs ? [ ],
-                env ? { },
-                ...
-              }:
-              {
-                nativeBuildInputs =
-                  with pkgs.buildPackages;
-                  [
-                    git
-                    cargo # with shell completions, instead of cargo-auditable
-                    cargo-insta # for updating insta snapshots
-                    clippy # more lints for better rust code
-                    nixfmt # for formatting nix code
-                  ]
-                  ++ nativeBuildInputs;
+          devShells.default =
+            (self.packages.${system}.hydra-check.override {
+              # prevent dependence on the source ./.; see ./package.nix
+              source = null;
+            }).overrideAttrs
+              (
+                {
+                  nativeBuildInputs ? [ ],
+                  env ? { },
+                  ...
+                }:
+                {
+                  nativeBuildInputs =
+                    with pkgs.buildPackages;
+                    [
+                      git
+                      cargo # with shell completions, instead of cargo-auditable
+                      cargo-insta # for updating insta snapshots
+                      clippy # more lints for better rust code
+                      nixfmt # for formatting nix code
+                    ]
+                    ++ nativeBuildInputs;
 
-                env =
-                  with pkgs.buildPackages;
-                  (removeAttrs env [ "RUST_LOG" ])
-                  // {
-                    # for developments, e.g. symbol lookup in std library
-                    RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
-                    # for debugging
-                    RUST_LIB_BACKTRACE = "1";
-                  };
-              }
-            );
-      }
-    );
+                  env =
+                    with pkgs.buildPackages;
+                    (removeAttrs env [ "RUST_LOG" ])
+                    // {
+                      # for developments, e.g. symbol lookup in std library
+                      RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+                      # for debugging
+                      RUST_LIB_BACKTRACE = "1";
+                    };
+                }
+              );
+        }
+      );
 }
